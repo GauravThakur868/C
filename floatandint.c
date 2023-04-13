7dd3d49dbdbcb3c8cf03cab3cc793754ecbc045a
GAURAV <gt267200@gmail.com>
2023-04-13 23:25:21 +0530

    2

---
+++ floatandint.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+	float a = 3.14;
+	int b = 15;
+	float c = 2*a*b;
+	int d = 2*a*b;
+	int e = (int)a;
+	printf("%f \n",c);
+	printf("%d \n",d);
+	printf("%d",e);
+	char f ='s';
+	printf("%d",f); 
+}

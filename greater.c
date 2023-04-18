1d4065cdae04f8644443b9d0c0b9f5e9589a4f81
GAURAV <gt267200@gmail.com>
2023-04-18 21:39:42 +0530

    3

---
+++ greater.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(){
+	int a,b,c;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	scanf("%d",&c);
+	if (a>b && a>c){
+		printf("%d is greatest",a);
+	}
+	else if(b>a && b>c){
+		printf("%d is gratest",b);
+	}
+	else{
+		printf("%d is greatest",c);
+	}
+}

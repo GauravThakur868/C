95cc8169e1a84f494c340a48340b9ad8acec3c2c
GAURAV <gt267200@gmail.com>
2023-04-19 20:59:57 +0530

    grading

---
+++ grade.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+	int a,b,c,d,e;
+	scanf("%d \n %d \n %d \n %d \n% d",&a,&b,&c,&d,&e);
+	int x = (a+b+c+d+e)/5;
+	if (x>=80 && x<=100){
+		printf("A");
+	}
+	else if(x>=60 && x<=79){
+		printf("B");
+	}
+	else if(x>=40 && x<=59){
+		printf("C");
+	}
+	else{
+		printf("D");
+	}
+}

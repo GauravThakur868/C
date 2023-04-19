f9547a927cb9e55b165a8f475dbda0de249acad2
GAURAV <gt267200@gmail.com>
2023-04-19 21:04:06 +0530

    middle

---
+++ middle.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main(){
+	int a,b,c;
+	scanf("%d\n%d\n%d",&a,&b,&c);
+	if(a>b && a>c){
+		if (b>c){
+			printf("%d",b);
+		}
+		else{
+			printf("%d",c);
+		}
+	}
+	else if(b>a && b>c){
+		if (a >c){
+			printf("%d",a);
+		}
+		else{
+			printf("%d",c);
+		}
+	}
+	else
+	{
+		if(a>b){
+			printf("%d",a);
+		}
+		else{
+			printf("%d",b);
+		}
+	
+	}
+}

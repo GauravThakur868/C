9ff20c2c55dd0e0dd3d1abb4ca2d7cfe8251644e
GAURAV <gt267200@gmail.com>
2023-04-22 23:12:55 +0530

    armstrong

---
+++ armstrong.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+void main(){
+	int a,b,c=0,d,count;   
+	printf("enter the number=");    
+	scanf("%d",&a);    
+	d=a;
+    while(a!=0){
+	a/10;
+	count=count+1;
+	}
+	printf("%d",count);
+	while(a>0)    
+	{    
+	b=a%10;    
+	c=c+pow(b,count);    
+	a=a/10;    
+	} 
+	if(d==c)    
+	printf("armstrong  number ");    
+	else    
+	printf("not armstrong number"); 
+}

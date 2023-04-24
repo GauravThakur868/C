2ab9664ef2322752d7de2075a506216e6cca388d
GAURAV <gt267200@gmail.com>
2023-04-24 23:26:34 +0530

    call by value

---
+++ callbyvalue.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int num(int n1,int n2,int n3);
+int main(){
+	int a,b,c;
+	printf("enter 3 numbers");
+	scanf("%d %d %d",&a,&b,&c);
+	printf("%d",num(a,b,c));
+}
+int num(int n1,int n2,int n3){
+	if (n1>n2 && n1>n3){
+		return n1;
+	}
+	else if (n2>n1 &&n2>n3){
+		return n2;
+	}
+	else{
+		return n3;
+	}
+}

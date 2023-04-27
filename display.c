ca3c6f005cc5a9f2a5c91e536ebc2139a14895f6
GAURAV <gt267200@gmail.com>
2023-04-27 23:13:10 +0530

    3

---
+++ display.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main(){
+	int size,i;
+	scanf("%d",&size);
+	int a[size],b[size];
+	for(i=0;i<size;i++){
+		scanf("%d",&a[i]);
+	}
+	for (i=0;i<size;i++){
+		b[i]=a[i]*2;
+	}
+	for (i=0;i<size;i++){
+		printf("%d",a[i]);
+	}
+	printf("\n");
+	for (i=0;i<size;i++){
+		printf("%d",b[i]);
+	}
+}

6b45c70fbe9a7d7c126c2e6a33122fd08b5f08dd
GAURAV <gt267200@gmail.com>
2023-04-29 21:55:43 +0530

    addition

---
+++ addition2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+// addition of 2 1-dimensional array and store value in new array
+void main(){
+	int x,i;
+	printf("size of array: ");
+	scanf("%d",&x);
+	int a[x],b[x],c[x];
+	printf("enter values for first: ");
+	for(i=0;i<x;i++){
+		scanf("%d",&a[i]);
+	}
+	printf("enter value for second: ");
+	for(i=0;i<x;i++){
+		scanf("%d",&b[i]);
+	}
+	for (i=0;i<x;i++){
+		c[i] = a[i]+b[i];
+	}
+	for(i=0;i<x;i++){
+		printf("%d\n",c[i]);
+	}
+}

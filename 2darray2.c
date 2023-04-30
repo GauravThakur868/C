5afce48c32a205c6a20122fef3854a5f475b3000
GAURAV <gt267200@gmail.com>
2023-04-30 21:27:24 +0530

    1

---
+++ 2darray2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main(){
+	int n,m;
+	printf("enter the no of rows: ");
+	scanf("%d",&n);
+	printf("enter the no of columns: ");
+	scanf("%d",&m);
+	int a[n][m],i,j;
+	for (i=0;i<n;i++){
+		for(j=0;j<m;j++){
+			printf("a[%d][%d]: ",i,j);
+			scanf("%d",&a[i][j]);
+		}
+	}
+for(i=0;i<n;i++){
+	for(j=0;j<m;j++){
+		printf("%d ",a[i][j]);
+		
+	}
+	printf("\n");
+}
+}

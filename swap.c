38131254323ea57919192f3392c6e529d7347aae
GAURAV <gt267200@gmail.com>
2023-05-01 22:54:19 +0530

    swapping

---
+++ swap.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+	int a,b,c=0;
+	printf("enter vlaue of a: ");
+	scanf("%d",&a);
+	printf("enter value of b: ");
+	scanf("%d",&b);
+	c = a;
+	a = b;
+	b = c;
+	printf("a = %d ",a);
+	printf("b = %d ",b);
+}

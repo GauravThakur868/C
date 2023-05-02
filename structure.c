4a0a1ae353526f0dd3383437c1643548f29f511c
GAURAV <gt267200@gmail.com>
2023-05-02 20:46:38 +0530

    structure

---
+++ structure.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+struct person{
+	char name[20];
+	int age;
+	struct student{
+		int regno;
+	}s;
+};
+int main(){
+	struct person p;
+	struct student s;
+	printf("enter th values\n");
+	scanf("%s",&p.name);
+	scanf("%d",&p.age);
+	scanf("%d",&p.s.regno);
+	printf("%s %d %d",p.name,p.age,p.s.regno);
+}

eb0bad7a60256ab2c3c870717cd63990e02973a0
GAURAV <gt267200@gmail.com>
2023-05-04 22:01:48 +0530

    1

---
+++ string3.c
@@ -0,0 +1,12 @@
+#include<string.h>
+int main(){
+	char st[]="123#ab0";
+	char st1[10];
+strncpy(st1,st,3);
+strcpy(st1,st);
+printf("%s",st1);
+printf("%d\n",strlen(st1));
+strcat(st1,st);
+printf("%s\n",st1);
+printf("%d",strcmp(st1,st,2));
+}

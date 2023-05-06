f7917fe70f013d3d278d57cf7aa434399c525aaa
GAURAV <gt267200@gmail.com>
2023-05-06 22:10:24 +0530

    specific index value

---
+++ stringvalue.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    char myString[] = "Hello world"; 
+    int index = 7; 
+    printf("The character at index %d is: %c\n", index, myString[index]); 
+    return 0;
+}

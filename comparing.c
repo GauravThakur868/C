0a1d0f59bf021cd30563b3c5eadf5544c9ea32bb
GAURAV <gt267200@gmail.com>
2023-05-12 22:26:36 +0530

    comparing 2 strings

---
+++ comparing.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main(){
+	char a[100], b[100];
+    int i, j = 0;
+
+    printf("Enter first string: ");
+    scanf("%s", &a);
+
+    printf("Enter second string: ");
+    scanf("%s", &b);
+
+    for(i = 0; a[i] != '\0' && b[i] != '\0'; i++) {
+        if(a[i] != b[i]) {
+            j = 1;
+            break;
+        }
+    }
+
+    if(j == 0 && a[i] == '\0' && b[i] == '\0')
+        printf("strings are equal\n");
+    else
+        printf("strings are not equal\n");
+}

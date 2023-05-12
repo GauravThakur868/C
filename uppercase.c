ddbfe9324bb5dbe8b9dfa3c581f4882dd7f0efb7
GAURAV <gt267200@gmail.com>
2023-05-12 22:25:22 +0530

    uppercase changing

---
+++ uppercase.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main(){
+	
+char a[100];
+   int i = 0;
+
+   printf("Enter a string: ");
+   scanf("%s", &a);
+
+   while (a[i]) {
+      if (a[i] >= 'a' && a[i] <= 'z') {
+         a[i] = a[i] - 32;
+      }
+      i++;
+   }
+   printf("Uppercase: %s", a);
+}

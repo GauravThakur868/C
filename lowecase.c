264d36d177effb58ddcd66d6513a6f024a058101
GAURAV <gt267200@gmail.com>
2023-05-13 21:59:42 +0530

    converting to lowecase

---
+++ lowecase.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+	char a[100];
+    int i;
+    printf("Enter a string: ");
+    scanf("%s", &a);
+
+    for(i = 0; a[i] != '\0'; i++) {
+        if(a[i] >= 'A' && a[i] <= 'Z') {
+            a[i] = a[i] + 32;
+        }
+    }
+    printf("lowercase: %s\n", a);
+}

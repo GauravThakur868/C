c0d9d9b9f5f54c72c2cb01d318a5dc6288012bf9
GAURAV <gt267200@gmail.com>
2023-05-20 22:33:52 +0530

    tan

---
+++ tan.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    double x, result;
+    printf("Enter the angle in degrees: ");
+    scanf("%lf", &x);
+    x = x * 3.14159 / 180.0;
+    result = tan(x);
+    printf("The tangent of %.2lf is %.4lf\n", x, result);
+    return 0;
+}

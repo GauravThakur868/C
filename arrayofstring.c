abc9d54f0ed529211681ff07ecdf34159db38c96
GAURAV <gt267200@gmail.com>
2023-07-07 10:46:59 +0530

    1

--- arrayofstring.c
+++ arrayofstring.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
-void main(){
+int main(){
     char names[3][10] = {"Abc", "Xyz", "Ijk"};
     for (int i = 0; i < 3; i++) {
         printf("Name %d: %s\n", i+1, names[i]);
     }
+    return 0;
 }

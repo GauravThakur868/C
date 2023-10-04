70e07cd2eb2ebbe1fe01c5d9467c3b066aabcac6
GAURAV <gt267200@gmail.com>
2023-10-04 21:40:03 +0530

    2

--- root.c
+++ root.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 #include <math.h>
-
 int main() {
     double number;
-    
-    // Prompt the user for input
     printf("Enter a number: ");
     scanf("%lf", &number);
-    
-    // Calculate and display the square root
     double squareRoot = sqrt(number);
     printf("Square root of %.2lf is %.2lf\n", number, squareRoot);
-    
-    // Calculate and display the cube root
     double cubeRoot = cbrt(number);
     printf("Cube root of %.2lf is %.2lf\n", number, cubeRoot);
     

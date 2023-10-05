fb92c2ae0a05b025b917a5b00cf4f92ab52ec749
GAURAV <gt267200@gmail.com>
2023-10-05 22:39:09 +0530

    2

--- x.c
+++ x.c
@@ -1,9 +1,72 @@
-#include<stdio.h>
-void first{
+#include <stdio.h>
+#include <stdlib.h>
 
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
 };
-int main(){
-    char c;
-    int x;
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Preorder traversal (Root -> Left -> Right)
+void preorderTraversal(struct Node* root) {
+    if (root == NULL)
+        return;
+    printf("%d ", root->data);
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
+
+// Inorder traversal (Left -> Root -> Right)
+void inorderTraversal(struct Node* root) {
+    if (root == NULL)
+        return;
+    inorderTraversal(root->left);
+    printf("%d ", root->data);
+    inorderTraversal(root->right);
+}
+
+// Postorder traversal (Left -> Right -> Root)
+void postorderTraversal(struct Node* root) {
+    if (root == NULL)
+        return;
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    printf("%d ", root->data);
+}
+
+int main() {
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+    root->right->left = createNode(6);
+    root->right->right = createNode(7);
+    root->left->left->left = createNode(8);
+    root->left->left->right = createNode(9);
+    root->right->right->left = createNode(10);
+    root->right->right->right = createNode(11);
+
+    printf("Preorder traversal: ");
+    preorderTraversal(root);
+    printf("\n");
+
+    printf("Inorder traversal: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    printf("Postorder traversal: ");
+    postorderTraversal(root);
+    printf("\n");
+
     return 0;
 }
